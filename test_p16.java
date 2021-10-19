118a40579effee7376c66edca24ddad289d190f3
Laixuzhen123 <a33909121@gmail.com>
2021-10-19 13:54:41 +0800

    first

---
+++ test_p16.java
@@ -0,0 +1,13 @@
+package test;
+
+public class test_p16 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println('A');
+		System.out.println("Åwªï¨Ï¥ÎJava!");
+		System.out.println(123);
+	}
+
+}
